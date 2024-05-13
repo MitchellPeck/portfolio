db4c55dcfa57b0fa7879a5a0421b11da3212bb6a
Mitchell Peck <50884550+MitchellPeck@users.noreply.github.com>
2024-05-13 17:10:17 -0400

    done

---
+++ src/lib/contentful.ts
@@ -0,0 +1,46 @@
+import contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
+
+export interface Project {
+  contentTypeId: "project";
+  fields: {
+    name: EntryFieldTypes.Text;
+    description: EntryFieldTypes.Text;
+    content: EntryFieldTypes.RichText;
+    image: EntryFieldTypes.Object;
+    fullImage: EntryFieldTypes.Object;
+    url: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    priority: EntryFieldTypes.Number;
+  };
+}
+
+export interface Post {
+  contentTypeId: "post";
+  fields: {
+    title: EntryFieldTypes.Text;
+    description: EntryFieldTypes.RichText;
+    content: EntryFieldTypes.RichText;
+    publishedDate: EntryFieldTypes.Date;
+    slug: EntryFieldTypes.Text;
+  };
+}
+
+export interface Experience {
+  contentTypeId: "experience";
+  fields: {
+    startDate: EntryFieldTypes.Date;
+    endDate: EntryFieldTypes.Date;
+    role: EntryFieldTypes.Text;
+    company: EntryFieldTypes.Text;
+    logo: EntryFieldTypes.Object;
+  };
+}
